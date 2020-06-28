78717d5fcb6826459c91e27ddb649cb03bf68c06
Fillipe Soares <felipesoares_1993@hotmail.com>
2020-06-27 21:04:20 -0300

    Configurando e iniciando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Contador de Pessoas",
+    debugShowCheckedModeBanner: false,
+    home: Container(
+      color: Colors.white,
+    ),
+  ));
+
+}
