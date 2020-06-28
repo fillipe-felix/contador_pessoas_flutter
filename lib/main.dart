b6776cff2479d24d7e7271274e9097792d56167c
Fillipe Soares <felipesoares_1993@hotmail.com>
2020-06-27 23:30:45 -0300

    Adicinei um widget Scaffold para adicionar uma appBar e bottomNavigation para resetar a tela

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,14 @@ class _HomeState extends State<Home> {
   int _people = 0;
   String _infoText = "Local vazio!";
 
-
   void _changePeople(int value) {
     setState(() {
-
       _people += value;
 
-      if(_people < 0 || _people == 0){
+      if (_people < 0 || _people == 0) {
         _infoText = "Local vazio";
         _people = 0;
-      } else if(_people <= 10){
+      } else if (_people <= 10) {
         _infoText = "Pode entrar";
       } else {
         _infoText = "Local lotado";
@@ -35,55 +33,83 @@ class _HomeState extends State<Home> {
     });
   }
 
+  void _reset() {
+    setState(() {
+      _people = 0;
+      _infoText = "Local vazio";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.asset(
-          "images/background.jpg",
-          fit: BoxFit.cover,
-          height: 1000.0,
-        ),
-        Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contador de Pessoas"),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: new BottomAppBar(
+        color: Colors.blue,
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              "Pessoa: $_people",
-              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            IconButton(
+              onPressed: () {
+                _reset();
+              },
+              icon: Icon(Icons.refresh),
+              color: Colors.white,
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  FlatButton(
-                    onPressed: () {
-                      _changePeople(1);
-                    },
-                    child: Text(
-                      "+1",
-                      style: TextStyle(fontSize: 40, color: Colors.white),
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      _changePeople(-1);
-                    },
-                    child: Text(
-                      "-1",
-                      style: TextStyle(fontSize: 40, color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Text(
-              _infoText,
-              style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
-            )
           ],
         ),
-      ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            "images/background.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoa: $_people",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 50),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        _changePeople(1);
+                      },
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                _infoText,
+                style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 50),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
