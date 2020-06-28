643ab68ff0aae1e364ba67baa6577cdc9b71fde7
Fillipe Soares <felipesoares_1993@hotmail.com>
2020-06-27 21:51:30 -0300

    Adicionando linhas(Row), botões sem fundo(FlatButton) e estruturando a hirarquia dos widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,25 @@ void main() {
           "Pessoa: 0",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {},
+              child: Text(
+                "+1",
+                style: TextStyle(fontSize: 40, color: Colors.white),
+              ),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text(
+                "-1",
+                style: TextStyle(fontSize: 40, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
         Text(
           "Pode entrar",
           style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
