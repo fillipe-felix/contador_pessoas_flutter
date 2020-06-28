1351fd5cb43985c466fe87bfc4c7cb01600d58ae
Fillipe Soares <felipesoares_1993@hotmail.com>
2020-06-27 22:19:24 -0300

    Adicionando Image, Padding e Stack para sobrepor os Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,36 +4,48 @@ void main() {
   runApp(MaterialApp(
     title: "Contador de Pessoas",
     debugShowCheckedModeBanner: false,
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    home: Stack(
       children: <Widget>[
-        Text(
-          "Pessoa: 0",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        Image.asset(
+          "images/background.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
         ),
-        Row(
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              onPressed: () {},
-              child: Text(
-                "+1",
-                style: TextStyle(fontSize: 40, color: Colors.white),
+            Text(
+              "Pessoa: 0",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
-            FlatButton(
-              onPressed: () {},
-              child: Text(
-                "-1",
-                style: TextStyle(fontSize: 40, color: Colors.white),
-              ),
-            ),
+            Text(
+              "Pode entrar",
+              style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
+            )
           ],
         ),
-        Text(
-          "Pode entrar",
-          style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
-        )
       ],
     ),
   ));
